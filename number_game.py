373ec9a2864c0222af17b404a04eb2ca83da37c4
leeledger <leeledger>
2022-06-30 20:40:46 +0900

    터틀 쓰레딩

--- number_game.py
+++ number_game.py
@@ -5,8 +5,7 @@ import random
 s = turtle.Screen()
 write = turtle.Turtle()
 write.hideturtle()
-s.bgcolor("black")
-write.color("white")
+
 playing = 1
 while playing:
     rand_numbers = [i for i in range(1,101)]
@@ -57,4 +56,3 @@ while playing:
     else:
         write.clear()
         playing = False
-
